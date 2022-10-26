516f8bd8744c7bed8570e92c1af2b999bc1b54b7
Alex <dinhhungvn@gmail.com>
2022-10-26 16:56:55 +0700

    First commit

---
+++ src/components/Home.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Layout from "@theme/Layout";
+import BlogPostItem from "@theme/BlogPostItem";
+import { Content } from "@theme/BlogPostPage";
+import PaginatorNavLink from "@theme/PaginatorNavLink";
+import { BlogPostProvider } from "@docusaurus/theme-common/internal";
+
+interface HomeProps {
+  readonly recentPosts: readonly { readonly content: Content }[];
+}
+
+function Home({ recentPosts }: HomeProps): JSX.Element {
+  return (
+    <Layout>
+      <div className="hero hero--dark hero--home shadow--lw">
+        <div className="container">
+          <div className="row">
+            <div className="col col--9 col--offset-1">
+              <h1 className="hero__title">👋 We are Fordeer Team.</h1>
+              <p className="hero__subtitle">Welcome to our development playground.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container margin-top--xl margin-bottom--lg">
+        <div className="row">
+          <div className="col col--9 col--offset-1">
+            {recentPosts.map(({ content: BlogPostContent }) => (
+              <BlogPostProvider key={BlogPostContent.metadata.permalink} content={BlogPostContent}>
+                <BlogPostItem>
+                  <BlogPostContent />
+                </BlogPostItem>
+              </BlogPostProvider>
+            ))}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col col--5 col--offset-5">
+            <PaginatorNavLink isNext permalink="/blog/page/2" title="Older Entries" />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default Home;
